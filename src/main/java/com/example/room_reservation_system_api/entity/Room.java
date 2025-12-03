b2ff3b52f80c3217df90d3da3b3f7df1d66a8848
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-04 02:40:35 +0900

    Modify Room,User to remove @OneToMany

--- src/main/java/com/example/room_reservation_system_api/entity/Room.java
+++ src/main/java/com/example/room_reservation_system_api/entity/Room.java
@@ -1,14 +1,9 @@
 package com.example.room_reservation_system_api.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,9 +17,6 @@ public class Room extends BaseEntity<Long> {
     private Long id;
 
     private String name;
-
-    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
-    private List<Reservation> reservations = new ArrayList<>();
 
     public static Room create(String name) {
         Room room = new Room();
--- src/main/java/com/example/room_reservation_system_api/entity/User.java
+++ src/main/java/com/example/room_reservation_system_api/entity/User.java
@@ -1,14 +1,7 @@
 package com.example.room_reservation_system_api.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -24,10 +17,6 @@ public class User extends BaseEntity<String> {
 
     private String name;
     private String email;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    @JsonIgnore()
-    private List<Reservation> reservations = new ArrayList<>();
 
     public static User create(String uid, String name, String email) {
         User user = new User();

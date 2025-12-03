f5d4c72809873a88b9a160e105b17224527a1784
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-04 00:51:52 +0900

    Initial commit

---
+++ src/main/java/com/example/room_reservation_system_api/RoomReservationSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.room_reservation_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationSystemApiApplication.class, args);
+	}
+
+}

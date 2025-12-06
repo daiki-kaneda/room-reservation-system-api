4d523a0fcd9e15ce384908f49a0e50c365c91ae4
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-06 23:46:21 +0900

    Modify SecurityConfig to permit only requests to /auth/**

--- src/main/java/com/example/room_reservation_system_api/config/security/SecurityConfig.java
+++ src/main/java/com/example/room_reservation_system_api/config/security/SecurityConfig.java
@@ -3,13 +3,22 @@ package com.example.room_reservation_system_api.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-        .build();
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                    auth -> 
+                    auth
+                    .requestMatchers("/auth/**")
+                    .permitAll()
+                    .anyRequest().authenticated())
+                .build();
     }
 }
